8d28bdc096deeef8ee8d3ba834deb0da86ffeaba
Happiness-AutoRepo <marat.metoff@gmail.com>
2023-10-16 09:24:25 -0400

    set up Spark reporter
    - set up reporting and incorporation of 508 into reports

--- src/main/java/runners/TestAccessibilityCompliance.java
+++ src/main/java/runners/TestAccessibilityCompliance.java
@@ -1,14 +1,22 @@
 package runners;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.List;
 
+import org.slf4j.Marker;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.markuputils.CodeLanguage;
+import com.aventstack.extentreports.markuputils.Markup;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.deque.html.axecore.results.Results;
 import com.deque.html.axecore.results.Rule;
 import com.deque.html.axecore.selenium.AxeBuilder;
@@ -23,33 +31,64 @@ import com.google.gson.JsonSyntaxException;
 import utilities.Driver;
 
 public class TestAccessibilityCompliance {
+	
+	ExtentReports extent;
+	ExtentTest test;
+	
+	@BeforeTest
+	public void setUp() {
+		extent = new ExtentReports();
+
+		ExtentSparkReporter spark = new ExtentSparkReporter("target/Spark.html");
+		extent.attachReporter(spark);
+		
+		test = extent.createTest("Acessibility test of the IRS landing page");
+	}
 
 	@Test
 	public void testOne() throws JsonIOException, JsonSyntaxException, FileNotFoundException {
-
+		
+		test.log(Status.INFO, "Navigating to the website");
 		Driver.getInstance().get("https://www.irs.gov");
 		
+		test.log(Status.INFO, "Analyzing 508 compliance");
 		AxeBuilder builder = new AxeBuilder();
 		Results results = builder.analyze(Driver.getInstance());
 		List<Rule> violations = results.getViolations();
 		
-		System.out.println(violations);
+		//Output violations to the console		
+		for (Rule rule : violations) {
+			System.out.println("=========================");
+			System.out.println(rule);
+		}
+		
 
-//		if (violations.size() == 0) {
-//			Assert.assertTrue(true, "No violations found");
-//		} else {
-//			JsonParser jsonParser = new JsonParser();
-//			Gson gson = new GsonBuilder().setPrettyPrinting().create();
-//			AxeReporter.writeResultsToJsonFile("filename", results);
-//			JsonElement jsonElement = jsonParser.parse(new FileReader("filename" + ".json"));
-//			String prettyJson = gson.toJson(jsonElement);
-//			AxeReporter.writeResultsToTextFile("filename", prettyJson);
-//			Assert.assertEquals(violations.size(), 0, violations.size() + " violations found");
-//		}
+		test.log(Status.INFO, "Checking if violations are present on the page");
+		//Output violations to .txt and .json file
+		if (violations.size() == 0) {
+			test.log(Status.PASS, "No violations found");
+			Assert.assertTrue(true, "No violations found");
+		} 
+		else {
+			test.log(Status.INFO, "Accessibility violations found.");
+			
+			JsonParser jsonParser = new JsonParser();
+			Gson gson = new GsonBuilder().setPrettyPrinting().create();
+			
+			AxeReporter.writeResultsToJsonFile("508results", results);
+			JsonElement jsonElement = jsonParser.parse(new FileReader("508results" + ".json"));
+			String prettyJson = gson.toJson(jsonElement);
+			
+			AxeReporter.writeResultsToTextFile("508results", prettyJson);
+			
+			test.log(Status.FAIL, MarkupHelper.createCodeBlock(prettyJson, CodeLanguage.JSON));
+			Assert.assertEquals(violations.size(), 0, violations.size() + " violations found");
+		}
 	}
 	
 	@AfterTest
 	public void wrapUp() {
 		Driver.closeDriver();
+		extent.flush();
 	}
 }
